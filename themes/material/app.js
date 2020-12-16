e8d1a9d4fe21aa5c778769f16b5a8ee1b5442b01
invoker-kael <63380399+invoker-kael@users.noreply.github.com>
2020-12-16 17:53:09 +0800

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -365,7 +365,7 @@ function append_files_to_list(path, files) {
       if ("|html|php|css|go|java|js|json|txt|sh|md|mp4|webm|avi|bmp|jpg|jpeg|png|gif|m4a|mp3|flac|wav|ogg|mpg|mpeg|mkv|rm|rmvb|mov|wmv|asf|ts|flv|pdf|".indexOf(`|${ext}|`) >= 0) {
         targetFiles.push(filepath);
         p += "";
-        c += " view";
+        c += "view";
       }
       html += `<li class="mdui-list-item file mdui-ripple" target="_blank"><a gd-type="${item.mimeType}" href="${p}" class="${c}">
 	          <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate" title="${item.name}">
